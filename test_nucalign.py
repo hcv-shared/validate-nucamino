efeeb9b58ebaa88776e94294845c6369fd62da0a
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-02-26 13:35:30 -0800

    Alignment tests work

--- test_nucalign.py
+++ test_nucalign.py
@@ -1,8 +1,10 @@
 import collections
 import functools
 import json
-import subprocess
+import os
 import random
+import secrets
+import subprocess
 import unittest
 
 import Bio.Seq
@@ -22,11 +24,16 @@ class NucAlignment(object):
             outp = nuc_proc.stdout.read()
             return json.loads(outp)
 
-    def __init__(self, infilename):
-        self.nuc_result = self.nucalign(infilename)
+    def __init__(self, infilename, gene):
+        self.nuc_result = self.nucalign(infilename, gene=gene)
 
-    def mutations(self, gene='NS3'):
-        return [result['Report']['Mutations'] for result in self.nuc_result[gene]]
+    def mutations(self):
+        for gene, results in self.nuc_result.items():
+            mtns = [mtn for r in results for mtn in r['Report']['Mutations']]
+            yield {
+                'gene': gene,
+                'mutations': mtns,
+            }
 
 
 class TestReferenceSequence(unittest.TestCase):
@@ -34,10 +41,12 @@ class TestReferenceSequence(unittest.TestCase):
     reference_filename = 'hcv1a.fasta'
 
     def test_no_mutations_in_reference(self):
-        alignment = NucAlignment(self.reference_filename)
-        muts = alignment.mutations()
-        for mut in muts:
-            self.assertEqual(len(mut), 0)
+        for gene in ['NS3', 'NS5A', 'NS5B']:
+            alignment = NucAlignment(self.reference_filename, gene=gene)
+            reports = list(alignment.mutations())
+            for report in reports:
+                for mut in list(report['mutations']):
+                    self.assertEqual(len(mut), 0)
 
 
 # ---------------------------------------------------------------------
@@ -45,20 +54,40 @@ class TestReferenceSequence(unittest.TestCase):
 GENE_POS = {
     'NS3': (3419, 5312),
     'NS5A': (6257, 7601),
-    'NS5B': (7602, 9374),
+    'NS5B': (7601, 9374),
 }
 
 NUCLEOTIDES = {'G', 'C', 'T', 'A'}
 
 
-Mutation = collections.namedtuple(
-    "Mutation",
-    ["pos", "sub_nt", "org_nt", "sub_cod", "org_cod"],
+_Mutation = collections.namedtuple(
+    "_Mutation",
+    ["nt_pos", "aa_pos", "sub_nt", "org_nt", "sub_cod", "org_cod", "gene"],
 )
 
+class Mutation(_Mutation):
 
-def random_gene_pos(gene='NS3'):
-    return random.choice(range(*GENE_POS[gene]))
+    @property
+    def org_aa(self):
+        return Bio.Seq.translate(self.org_cod)
+
+    @property
+    def sub_aa(self):
+        return Bio.Seq.translate(self.sub_cod)
+
+    def __str__(self):
+        sub = "{} {} {} {}".format(
+            self.gene,
+            self.org_aa,
+            self.aa_pos,
+            self.sub_aa,
+        )
+        return "<Mutation {}>".format(sub)
+
+
+def gene_seq(seq, gene=None):
+    start, end = GENE_POS[gene]
+    return seq[start:end]
 
 
 def random_substitution(orig):
@@ -75,22 +104,26 @@ def codon_at(idx, seq):
 
 
 def random_mutation(seq, gene='NS3'):
-    idx = random_gene_pos(gene)
-    orig_nt = seq[idx]
-    orig_codon = codon_at(idx, seq)
+    start, end = GENE_POS[gene]
+    gseq = gene_seq(seq, gene=gene)
+    idx = random.choice(range(len(gseq)))
+    orig_nt = gseq[idx]
+    orig_codon = codon_at(idx, gseq)
     sub = random_substitution(orig_nt)
     sub_codon = orig_codon[:idx%3] + sub + orig_codon[(idx%3)+1:]
     return Mutation(
-        pos=idx,
+        nt_pos=idx+start,
+        aa_pos=idx//3 + 1,
         org_nt=orig_nt,
         org_cod=orig_codon,
         sub_nt=sub,
         sub_cod=sub_codon,
+        gene=gene,
     )
 
 
 def apply_mutation(mut, sequence):
-    idx = mut.pos
+    idx = mut.nt_pos
     return (
         sequence[:idx]
         + mut.sub_nt
@@ -152,11 +185,6 @@ class TestTranslation(unittest.TestCase):
 
 class TestRandomness(unittest.TestCase):
 
-    def test_gene_pos(self):
-        for _ in range(1000):
-            ridx = random_gene_pos('NS3')
-            self.assertIn(ridx, range(3420, 5474))
-
     def test_random_substitution(self):
         for _ in range(1000):
             orig = random.choice(list(NUCLEOTIDES))
@@ -174,16 +202,17 @@ class TestRandomness(unittest.TestCase):
             ""
         )
         for _ in range(100):
-            mut = random_mutation(seq)
+            gene = random.choice(["NS3", "NS5A", "NS5B"])
+            mut = random_mutation(seq, gene=gene)
             applied = apply_mutation(mut, seq)
             self.assertEqual(
                 mut.org_nt,
-                seq[mut.pos],
+                seq[mut.nt_pos],
                 "Expected mutation to contain the original nucleotide",
             )
             self.assertEqual(
                 mut.sub_nt,
-                applied[mut.pos],
+                applied[mut.nt_pos],
                 "Expected new sequence to contain the mutation",
             )
 
@@ -209,8 +238,9 @@ class TestRandomness(unittest.TestCase):
 def make_mut_in_file(infile, outfile='hcv1a_mut.fasta'):
     with open(infile, 'r') as inf:
         inf_header, inf_seq = inf.readlines()
-    mutation = random_mutation(inf_seq)
-    idx = mutation.pos
+    gene = random.choice(["NS3", "NS5A", "NS5B"])
+    mutation = random_mutation(inf_seq, gene=gene)
+    idx = mutation.nt_pos
     outf_header = inf_header.strip() + " with mutation: {}\n".format(mutation)
     outf_seq = apply_mutation(mutation, inf_seq)
     with open(outfile, 'w') as outf:
@@ -222,63 +252,78 @@ class TestSimpleSubstitutions(unittest.TestCase):
 
     reference_file = "hcv1a.fasta"
     tmp_filename = "hcv1a_mut.fasta"
-    iterations = 10
+    iterations = 1000
 
-    def check_simple_sub(self):
+    def setUpClass():
+        seed = os.getenv('NUCALIGN_SEED')
+        if seed is None:
+            seed = secrets.token_hex()
+        print("Initializing random with seed: {}".format(seed))
+        random.seed(seed)
+
+    def align_simple_sub(self):
         mutation = make_mut_in_file(
             self.reference_file,
             outfile=self.tmp_filename,
         )
-        alignment = NucAlignment(self.tmp_filename)
+        alignment = NucAlignment(self.tmp_filename, gene=mutation.gene)
         return mutation, alignment
 
-    def test_simple_sub(self):
-        print()
-        hdr = "VirtMut         FoundMut            Diff"
-        print(hdr)
+    def check_vmtn_mtn(self, vmtn, mtn):
+        self.assertEqual(vmtn.aa_pos, mtn['Position'])
+        self.assertEqual(vmtn.org_aa, mtn['ReferenceText'])
+        self.assertEqual(vmtn.sub_aa, mtn['AminoAcidText'])
+        self.assertIn(
+            vmtn.nt_pos - mtn['NAPosition'],
+            {-1, 0, 1},
+        )
 
-        def format_aln_mutation(m):
-            if m is None:
-                return ""
-            return "{} {} {} (nt:{})".format(
-                m["ReferenceText"],
-                m["Position"],
-                m["AminoAcidText"],
-                m["NAPosition"],
-            )
+    def check_mutation_report(self, alignment, gene=None):
+        reports = list(
+            r for r in alignment.mutations()
+            if len(r['mutations']) > 0
+        )
+        self.assertEqual(len(reports), 1, "Expected a single mutation report")        
+        report = next(iter(reports))
+        self.assertEqual(
+            len(report['mutations']),
+            1,
+            "Expected a single mutation",
+        )
+        self.assertEqual(gene, report['gene'])
+        return report
 
-        def format_virt_mutation(mtn):
-            return "{} {} {}".format(
-                mtn.org_nt.lower(),
-                mtn.pos,
-                mtn.sub_nt.lower(),
-            )
+    def check_synonymous_report(self, alignment, gene=None):
+        reports = list(alignment.mutations())
+        for rep in reports:
+            msg = "Unexpected mutation in {}".format(rep)
+            self.assertFalse(rep['mutations'], msg)
 
-        def pos_diff(virt, aln):
-            if virt is None or aln is None:
-                return "-"
-            return virt.pos - aln["NAPosition"]
+    def check_simple_sub(self):
+        vmtn, aln = self.align_simple_sub()
+        if vmtn.org_aa == vmtn.sub_aa:
+            self.check_synonymous_report(aln, vmtn.gene)
+            return
+        try:
+            report = self.check_mutation_report(aln, gene=vmtn.gene)
+        except AssertionError as e:
+            print(list(aln.mutations()))
+            print(vmtn)
+            print(vmtn.org_aa)
+            print(vmtn.sub_aa)
+            raise e
+        mtn = next(iter(report['mutations']))
+        try:
+            self.check_vmtn_mtn(vmtn, mtn)
+        except AssertionError as e:
+            print(e)
+            print(mtn)
+            print(aln)
+            raise e
 
-        def format_codons(org, sub):
-            return "{}({}) -> {}({})".format(
-                org,
-                Bio.Seq.translate(org),
-                sub,
-                Bio.Seq.translate(sub),
-            )
-
-        tmpl = "{virtmut: <16}{foundmut: <20}{diff: <16}"
-        for i in range(1):
-            mtn, aln = self.check_simple_sub()
-            mutations = aln.mutations()
-            assert len(mutations) == 1
-            mutations = mutations[0]  # 😢
-            assert len(mutations) < 2
-            mutation = next(iter(mutations), None)
-            data = {
-                "virtmut": format_virt_mutation(mtn),
-                "foundmut": format_aln_mutation(mutation),
-                "diff": pos_diff(mtn, mutation),
-            }
-            outp = tmpl.format(**data)
-            print(outp)
+    # @unittest.skip("Takes too long")
+    def test_simple_subs(self):
+        for i in range(self.iterations):
+            if i % 20 == 0 and i > 0:
+                print(i)
+            self.check_simple_sub()
