dca2418b472a1b23d331f479dda527eaac799eff
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-03-19 11:14:47 -0700

    Remove pytest.el

--- pytest.el
+++
@@ -1,8 +0,0 @@
-(defun pytest (root-dir buffer-name)
-    (compile "python3.6 -m unittest && date"))
-
-(defun run-pytest ()
-  (interactive)
-  (pytest "/home/nathaniel/tmp/validate-nucamino" "*pytest*"))
-
-(global-set-key (kbd "<f5>") 'run-pytest)
