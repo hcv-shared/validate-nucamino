8963fd3600e0f3f6bef8e446d9ff69551b767bc0
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-03-19 14:12:26 -0700

    Update reference sequence test to cover all genotypes

--- test_reference_seqs.py
+++ test_reference_seqs.py
@@ -1,14 +1,27 @@
 import common
 import unittest
 
+import Bio.SeqIO as seqio
+
 class TestReferenceSequence(unittest.TestCase):
 
-    reference_filename = 'hcv1a.fasta'
+    references_filename = 'hcv-refs.fasta'
+    genes = ('NS3', 'NS5A', 'NS5B')
 
     def test_no_mutations_in_reference(self):
-        for gene in ['NS3', 'NS5A', 'NS5B']:
-            alignment = common.NucAlignment(self.reference_filename, gene=gene)
-            reports = list(alignment.mutations())
-            for report in reports:
-                for mut in list(report['mutations']):
-                    self.assertEqual(len(mut), 0)
+        gene = ','.join(self.genes)
+        with open(self.references_filename) as inf:
+            refseqs = seqio.parse(inf, 'fasta')
+            for seq in refseqs:
+                genotype = seq.description.split()[1]
+                profile = 'hcv' + genotype
+                alignment = common.NucAlignment(
+                    str(seq.seq),
+                    profile=profile,
+                    gene=gene,
+                )
+                reports = list(alignment.mutations())
+                for report in reports:
+                    muts = list(report['mutations'])
+                    msg = "Found mutations in the reference {}".format(genotype)
+                    self.assertEqual(len(muts), 0, msg)
